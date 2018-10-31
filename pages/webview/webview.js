82c7c2919fcba353021efb343d9dca8791b6b894
ourzizz <552716481@qq.com>
2018-11-01 00:59:06 +0800

    收藏功能的确不好写，今天完成已登录用户的取消收藏功能，明天晚上完成新增收藏，涉及到了新用户的登录授权

--- pages/webview/webview.js
+++ pages/webview/webview.js
@@ -14,7 +14,8 @@ Page({
         userInfo: {},
         logged: false,
         takeSession: false,
-        requestResult: ''
+        requestResult: '',
+        collect: {'status':false,'src':'/image/collect.png','text':'收藏本文'}
     },
     onShareAppMessage: function() {
         console.log("inshare")
@@ -31,22 +32,49 @@ Page({
             this.data.tabs.push([webJson.notify[key].typename, webJson.notify[key].content])
         }
     },
+    renderCollect:function(){
+        const session = qcloud.Session.get()
+        var that = this
+        if(session){
+            this.data.userInfo = session.userinfo
+            qcloud.request({
+                url: `${config.service.host}/weapp/demo/get_if_userhasfile/` + session.userinfo.openId + `/` + this.data.fileid,
+                success(res) {
+                    if(res.data["userhasfile"] != false)
+                    {
+                        that.setData({
+                            collect: { 'status':true,'src':'/image/collected.png','text':'已收藏'},
+                        })
+                    }
+                    else{
+                        that.setData({
+                            collect: { 'status':false,'src':'/image/collect.png','text':'收藏本文'}
+                        })
+                    }
+                },
+                fail(error) {
+                    util.showModel('请求失败', error);
+                    console.log('request fail', error);
+                }
+            })
+
+        }
+    },
     onLoad: function(options) {
         var that = this;
         this.data.fileid = options.fileid 
+        this.renderCollect()
         util.showBusy('请求中...')
         qcloud.request({
             url: `${config.service.host}/weapp/demo/get_filepage_json/` + options.fileid,
             success(result) {
                 util.showSuccess('请求成功完成')
                 that.initTabs(result.data)
-
                 //上一步获取数据并初始化navmap后需要渲染nav
                 wx.getSystemInfo({ //设置导航栏平分
                     success: function(res) {
                         that.setData({
                             tabs: that.data.tabs,
-                            //tabs: that.data.navMap,
                             sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
                             sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
                         });
@@ -91,38 +119,54 @@ Page({
         })
         console.log(this.data.userInfo)
     },
+    cancleCollect:function(openId,fileId)
+    {//取消收藏
+        var that = this
+        qcloud.request({
+            url: `${config.service.host}/weapp/demo/delete_user_file/` + openId + `/` + fileId,
+            success(res) {
+                    that.setData({
+                        collect: { 'status':false,'src':'/image/collect.png','text':'收藏本文'}
+                    })
+            },
+            fail(error) {
+                util.showModel('请求失败', error);
+                console.log('request fail', error);
+            }
+        })
+    },
     collectFile:function ()
     {
-        if (this.data.logged) return
         var that = this
-        util.showBusy('正在登录')
-
-        const session = qcloud.Session.get()
-
-        if (session) {
-            qcloud.loginWithCode({
-                success: res => {
-                    this.setData({ userInfo: res, logged: true })
-                    util.showSuccess('登录成功')
-                    console.log(this.data.userInfo)
-                },
-                fail: err => {
-                    console.error(err)
-                    util.showModel('登录错误', err.message)
-                }
-            })
-        } else {
-            qcloud.login({
-                success: res => {
-                    this.setData({ userInfo: res, logged: true })
-                    util.showSuccess('登录成功')
-                },
-                fail: err => {
-                    console.error(err)
-                    util.showModel('登录错误', err.message)
-                }
-            })
+        if(this.data.collect['status'] == true)
+        {
+            //const session = qcloud.Session.get()
+            this.cancleCollect(this.data.userInfo.openId,this.data.fileid)
         }
+        //if (session) {
+        //qcloud.loginWithCode({
+        //success: res => {
+        //this.setData({ userInfo: res, logged: true })
+        //util.showSuccess('登录成功')
+        //console.log(this.data.userInfo)
+        //},
+        //fail: err => {
+        //console.error(err)
+        //util.showModel('登录错误', err.message)
+        //}
+        //})
+        //} else {
+        //qcloud.login({
+        //success: res => {
+        //this.setData({ userInfo: res, logged: true })
+        //util.showSuccess('登录成功')
+        //},
+        //fail: err => {
+        //console.error(err)
+        //util.showModel('登录错误', err.message)
+        //}
+        //})
+        //}
     },
 
     // 切换是否带有登录态
@@ -136,8 +180,8 @@ Page({
     {
         const session = qcloud.Session.get()
         console.log(session)
-      wx.switchTab({
-        url: '../index/index'
-      })
+        wx.switchTab({
+            url: '../index/index'
+        })
     }
 });
