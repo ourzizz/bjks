3c8746ef5df77c359cc46a5372d59a32a97ac424
qc_c32e99be72894fada303f12679352a3b <qc_c32e99be72894fada303f12679352a3b@git.code.tencent.com>
2019-01-07 17:23:19 +0800

    完成地址省市县选择器，遇到问题，首先要想到官方资料，卡了好几天，官方文档几句话就完了

--- pages/user_address/user_address.js
+++ pages/user_address/user_address.js
@@ -1,76 +1,12 @@
-const app = getApp()
-const date = new Date()
-const years = []
-const months = []
-const days = []
-
-for (let i = 1990; i <= date.getFullYear(); i++) {
-  years.push(i)
-}
-
-for (let i = 1; i <= 12; i++) {
-  months.push(i)
-}
-
-for (let i = 1; i <= 31; i++) {
-  days.push(i)
-}
-
 Page({
-  onShareAppMessage() {
-    return {
-      title: 'picker-view',
-      path: 'page/component/pages/picker-view/picker-view'
-    }
-  },
-
   data: {
-    years,
-    year: date.getFullYear(),
-    months,
-    month: 2,
-    days,
-    day: 2,
-    value: [9999, 1, 1],
-    isDaytime: true,
-    area_parent_sonlist:[],
-      provincies:[],
-      cities:[],
-      counties:[],
+    region: ['贵州省', '毕节市', '七星关区'],
+    //customItem: '全部'
   },
-  get_son_list()  
-    {},
-
-  bindChange(e) {
-    const val = e.detail.value
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      year: this.data.years[val[0]],
-      month: this.data.months[val[1]],
-      day: this.data.days[val[2]],
-      lsDaytime: !val[3]
+      region: e.detail.value
     })
-  },
-
-  onQuery: function () {//大小写敏感
-    wx.cloud.init()
-    const db = wx.cloud.database()
-    db.collection('area').where({
-      PARENT_ID: "0"
-    }).get({
-      success: res => {
-        this.setData({
-          queryResult: JSON.stringify(res.data, null, 2)
-        })
-        console.log('[数据库] [查询记录] 成功: ', res)
-      },
-      fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
-        console.error('[数据库] [查询记录] 失败：', err)
-      }
-    })
-  },
-
+  }
 })
