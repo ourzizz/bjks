0a89c7368791519d9021eea873f8824285f18e36
qc_c32e99be72894fada303f12679352a3b <qc_c32e99be72894fada303f12679352a3b@git.code.tencent.com>
2019-01-14 18:46:44 +0800

    开发address模块ing

--- pages/user_address/user_address.js
+++ pages/user_address/user_address.js
@@ -7,12 +7,14 @@ Page({
     step:1,
     address_list:[],
     new_address:{},
+    open_id:''
   },
+
   onLoad:function(option) {//o9pU65LTYEE8tVWQR_yClRc1466k
-    open_id = option.open_id
+    this.data.open_id = option.open_id
     let that = this
     qcloud.request({
-      url: `${config.service.host}/user_address/get_user_all_address/` + open_id,
+      url: `${config.service.host}/user_address/get_user_all_address/` + this.data.open_id,
       success(result) {
         util.showSuccess('请求成功完成')
         that.setData({
@@ -21,9 +23,9 @@ Page({
       }
     })
   },
+
   bindRegionChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
-    // let new_address = {}
     this.data.new_address.province = e.detail.value[0]
     this.data.new_address.city = e.detail.value[1]
     this.data.new_address.county = e.detail.value[2]
@@ -31,32 +33,27 @@ Page({
       region: e.detail.value,
     })
   },
-  next_step:function(){
+
+  edit_address: function (event) {//用户点击编辑
+    let idx = event.currentTarget.dataset.idx
+    this.data.new_address = JSON.parse(JSON.stringify(this.data.address_list[idx]));
     this.setData({
-      step:2
-    })
-  },
-  user_add_address:function(){
-    console.log('insert new address into databases')
-  },
-  pre_step: function () {
-    console.log("pre")
-    this.data.address_list.push(this.data.new_address)
-    this.user_add_address
-    this.setData({
-      step: 1,
-      address_list:this.data.address_list
+      step:2,
+      new_address:this.data.new_address
     })
   },
 
   input_name:function(e){
     console.log(e.detail.value)
-    this.data.new_address.name = e.detail.value
+    if(e.detail.value != null){
+      this.data.new_address.name = e.detail.value
+    }
   },
   
   reg_tel:function(phoneno){
     return true;
   },
+
   input_telphone:function(e){//input失去焦点才触发，bindinput是每输入一个字符就触发一次
     console.log("intelphone")
     if(this.reg_tel(e.detail.value)){
@@ -66,8 +63,62 @@ Page({
   },
 
   input_detail:function(e){
-    this.data.new_address.detail = e.detail.value
+    if(e.detail.value != null){
+      this.data.new_address.detail = e.detail.value
+    }
   },
+
+  delete_address: function (event) {//用户点击删除
+    let idx = event.currentTarget.dataset.idx
+    this.data.new_address = JSON.parse(JSON.stringify(this.data.address_list[idx]));
+    this.setData({
+      step:2,
+      new_address:this.data.new_address
+    })
+  },
+
+  create_address:function(){//点击新增，进入步骤2，不携带任何参数
+    this.data.new_address = {} //这里的意思需要新分配内存
+    this.setData({
+      step:2,
+      new_address:this.data.new_address
+    })
+  },
+
+  insert_address:function(){
+    console.log('insert new address into databases')
+  },
+
+  submit: function () {
+    let that = this
+    if(this.data.new_address.address_id != null) {//addressid存在表示这是保存编辑，编辑后modify[{key:1,value:'exmple'}...]后台根据key选择不同的操作将value update到数据库
+      // var modify = this.get_change()//得到modify数组
+      // this.update_address(modify)
+      console.log('submit');
+      that.setData({
+        step: 1,
+        address_list: that.data.address_list
+      })
+    } else {//id不存在表示新建的
+      console.log("pre")
+      this.data.address_list.push(this.data.new_address)
+      let address = JSON.stringify(this.data.new_address)
+      qcloud.request({
+        url: `${config.service.host}/weapp/user_address/user_add_address/` + this.data.open_id + '/' + address,
+        success(result) {
+          that.setData({
+            step: 1,
+            address_list: that.data.address_list
+          })
+        },
+        fail(error) {
+          util.showModel('请求失败', error);
+          console.log('request fail', error);
+        }
+      })
+    }
+  },
+
 
   select_default_address_back_to_prepage(e) {//返回用户选择的地址给父页面
     console.log('radio发生change事件，携带value值为：', e.detail.value)
@@ -81,5 +132,6 @@ Page({
     wx.navigateBack({
       delta: 1
     })
-  }
+  },
+
 })
