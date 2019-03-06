fd12fce210088a52ad2c9bc85a533d47fa8c6197
qc_c32e99be72894fada303f12679352a3b <qc_c32e99be72894fada303f12679352a3b@git.code.tencent.com>
2019-03-06 17:39:41 +0800

    分类页面自动加载完成，诺必达页面美化ing

--- pages/bookstore/bookstore.js
+++ pages/bookstore/bookstore.js
@@ -3,262 +3,24 @@
 //本页设计思路
 //数据格式 分类数据 统一为{parent_id,son_list} 每个对象都有自身id和子列表
 //渲染一级目录的时候 在onLoad函数  nav_list: this.data.parent_son[0].son_list
-//点击触发渲染时，调用get_sons_by_id 
+//需要解决，某个商品是父类子类都应该有的，那么设置它的class_id为父类的，拿到叶子id请求的时候
+//后台应该给出从根到叶的所有商品列表
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
 var util = require('../../utils/util.js')
 
+function get_sons_by_id (parent_son,class_id) {
+    for (var key in parent_son) {
+        if (parent_son[key].parent_id == class_id) {
+            return parent_son[key].son_list
+        }
+    }
+    return 'NULL'
+}
+
 Page({    
     data: {
-        parent_son: [{//{{{
-            "parent_id": "99",
-            "son_list": [{
-                "class_id": "100",
-                "class_name": "公务员",
-                "count_sons": "3"
-            }, {
-                "class_id": "101",
-                "class_name": "事业单位",
-                "count_sons": "1"
-            }, {
-                "class_id": "102",
-                "class_name": "经济师",
-                "count_sons": "32"
-            }, {
-                "class_id": "103",
-                "class_name": "职称计算机",
-                "count_sons": "0"
-            }, {
-                "class_id": "104",
-                "class_name": "建筑职称",
-                "count_sons": "0"
-            }, {
-                "class_id": "105",
-                "class_name": "一建",
-                "count_sons": "12"
-            }, {
-                "class_id": "106",
-                "class_name": "消防工程",
-                "count_sons": "0"
-            }, {
-                "class_id": "107",
-                "class_name": "执业药师",
-                "count_sons": "0"
-            }]
-        }, {
-            "parent_id": "100",
-            "son_list": [{
-                "class_id": "108",
-                "class_name": "行测",
-                "count_sons": "0"
-            }, {
-                "class_id": "109",
-                "class_name": "申论",
-                "count_sons": "0"
-            }, {
-                "class_id": "110",
-                "class_name": "面试",
-                "count_sons": "0"
-            }]
-        }, {
-            "parent_id": "101",
-            "son_list": [{
-                "class_id": "113",
-                "class_name": "事业单位考试",
-                "count_sons": "0"
-            }]
-        }, {
-            "parent_id": "102",
-            "son_list": [{
-                "class_id": "111",
-                "class_name": "中级",
-                "count_sons": "15"
-            }, {
-                "class_id": "112",
-                "class_name": "初级",
-                "count_sons": "15"
-            }]
-        }, {
-            "parent_id": "111",
-            "son_list": [{
-                "class_id": "114",
-                "class_name": "工商管理专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "115",
-                "class_name": "人力资源专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "116",
-                "class_name": "金融专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "117",
-                "class_name": "财政税收专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "118",
-                "class_name": "房地产经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "119",
-                "class_name": "建筑经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "120",
-                "class_name": "商业经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "121",
-                "class_name": "农业经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "122",
-                "class_name": "保险专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "123",
-                "class_name": "邮电经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "124",
-                "class_name": "旅游经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "125",
-                "class_name": "运输公路专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "126",
-                "class_name": "运输民航专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "127",
-                "class_name": "运输水路专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "128",
-                "class_name": "运输铁路专业",
-                "count_sons": "0"
-            }]
-        }, {
-            "parent_id": "112",
-            "son_list": [{
-                "class_id": "129",
-                "class_name": "工商管理专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "130",
-                "class_name": "人力资源专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "131",
-                "class_name": "金融专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "132",
-                "class_name": "财政税收专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "133",
-                "class_name": "房地产经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "134",
-                "class_name": "建筑经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "135",
-                "class_name": "商业经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "136",
-                "class_name": "农业经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "137",
-                "class_name": "保险专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "138",
-                "class_name": "邮电经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "139",
-                "class_name": "旅游经济专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "140",
-                "class_name": "运输公路专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "141",
-                "class_name": "运输民航专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "142",
-                "class_name": "运输水路专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "143",
-                "class_name": "运输铁路专业",
-                "count_sons": "0"
-            }]
-        }, {
-            "parent_id": "105",
-            "son_list": [{
-                "class_id": "149",
-                "class_name": "一建建筑工程专业",
-                "count_sons": "2"
-            }, {
-                "class_id": "150",
-                "class_name": "一建公路专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "151",
-                "class_name": "一建铁路专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "152",
-                "class_name": "一建民航机场专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "153",
-                "class_name": "一建港口与航道专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "154",
-                "class_name": "一建水利水电专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "155",
-                "class_name": "一建市政公用专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "156",
-                "class_name": "一建通信与广电专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "157",
-                "class_name": "一建矿业工程专业",
-                "count_sons": "0"
-            }, {
-                "class_id": "158",
-                "class_name": "一建机电工程专业",
-                "count_sons": "0"
-            }]
-        }, {
-            "parent_id": "149",
-            "son_list": [{
-                "class_id": "159",
-                "class_name": "建筑专业优惠套餐",
-                "count_sons": "0"
-            }, {
-                "class_id": "160",
-                "class_name": "建筑工程专业单本图书",
-                "count_sons": "0"
-            }]
-        }],//}}}
+        parent_son: [],//}}}
         requestResult: '',
         canIUseClipboard: wx.canIUse('setClipboardData'),
         activeIndex: 0,
@@ -272,17 +34,30 @@ Page({
         nav_2:[],
         nav_3:[],
     },
-    onLoad: function () {
-        var son_list = this.get_sons_by_id("100")//100是公务员 下阶段 这里需要自动生成
-        var id = this.find_first_leave("100")
-        this.show_goods_list_by_class_id(id)
-        this.setData({
-            nav_list: this.data.parent_son[0].son_list,
-            nav_2: son_list,
+
+    onLoad:function (){
+        let that = this
+        qcloud.request({
+            url: `${config.service.host}/goods_class/get_parent_son`,
+            success(result) {
+                that.data.parent_son = result.data
+                var son_list = get_sons_by_id(result.data,"100")//100是公务员 下阶段 这里需要自动生成
+                that.setData({
+                    parent_son:result.data,
+                    nav_list: result.data[2].son_list,
+                    nav_2: son_list,
+                })
+                var id = that.find_first_leave("100")
+                that.show_goods_list_by_class_id(id)
+                that.drawing_selected_item(0,'100')
+                that.drawing_selected_item(1,id)
+                that.get_sum_cart()
+            },
+            fail(error) {
+                util.showModel('请求失败', error);
+                console.log('request fail', error);
+            }
         })
-        this.drawing_selected_item(0,'100')
-        this.drawing_selected_item(1,id)
-        this.get_sum_cart()
     },
 
     /*
@@ -292,7 +67,7 @@ Page({
     show_goods_list_by_class_id: function (class_id) {
         let that = this
         qcloud.request({
-            url: `${config.service.host}/weapp/goods/get_goods_json_by_class_id/` + class_id,
+            url: `${config.service.host}/goods_class/get_goods_list_by_class_id/` + class_id,
             success(result) {
                 that.setData({
                     goods_list: result.data
@@ -322,7 +97,7 @@ Page({
     show_next_nav: function (event) {
         var class_id = event.currentTarget.dataset.class_id
         var layer = event.currentTarget.dataset.layer
-        var son_list = this.get_sons_by_id(class_id)
+        var son_list = get_sons_by_id(this.data.parent_son,class_id)
         var leave_id = this.find_first_leave(class_id)
         var first_son = this.get_first_son(class_id)
         this.show_goods_list_by_class_id(leave_id)//默认显示的是目录中的第一个类别的商品
@@ -352,14 +127,6 @@ Page({
 
     },
 
-    get_sons_by_id: function (class_id) {
-        for (var key in this.data.parent_son) {
-            if (this.data.parent_son[key].parent_id == class_id) {
-                return this.data.parent_son[key].son_list
-            }
-        }
-        return 'NULL'
-    },
 
     get_first_son: function (pid) {//获得给定id的第一个儿子，不存在返回NULL
         for (var key in this.data.parent_son) {
