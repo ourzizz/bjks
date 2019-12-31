cd7c61bd4809103b3ae0609cea78e2dbe99d3906
ourzizz <552716481@qq.com>
2019-12-31 18:23:35 +0800

    似乎倒霉的事儿都他妈我占全了，备份上传

--- pages/baoming/kaoshengInfo/kaoshengInfo.js
+++ pages/baoming/kaoshengInfo/kaoshengInfo.js
@@ -3,23 +3,22 @@ var qcloud = require('../../../vendor/wafer2-client-sdk/index')
 var config = require('../../../config')
 var util = require('../../../utils/util.js')
 var cls = require('../../../utils/myclass.js')
+var cosPath = "http://bjks-1252192276.cos.ap-chengdu.myqcloud.com"
 var app = getApp()
 Page({
     data: {
         kaosheng_flg:"new",
-        kaoshengInfo: {},
+        kaoshengInfo: {
+            photoUrl:"null"
+        },
         userInfo:{},
         logged:false,
-        localImagePaths: [], //小程序wxml页面显示的图片列表,如果是修改那么就是从服务器请求的url，否则为本地选中的图片列表
-        imageNameList: [], //upload返回的是url和name，以前开发保留了name用逗号连接存到数据库，这个列表是存储使用的,服务器发来的namelist
-        tempFilePaths:[], //待上传列表
+        localImagePath:'', //小程序wxml页面显示的图片列表,如果是修改那么就是从服务器请求的url，否则为本地选中的图片列表
+        imageName:'', //upload返回的是url和name，以前开发保留了name用逗号连接存到数据库，这个列表是存储使用的,服务器发来的namelist
+        tempFilePath:'', //待上传列表
         pub_lock:'unlock',//发布锁，避免用户多次点击 unlock lock
         minganStatus:true
     },
-
-    //如果调用页面的时候给出的是messageId表示需要修改信息，拿messageid取后台初始化
-    init_kaosheng: function (msg_id) {//{{{
-    },//}}}
 
     onLoad: function (options) {//{{{ 需要向后台请求考生基本信息 和报考信息
         let that = this
@@ -45,6 +44,7 @@ Page({
                         kaoshengInfo:{}
                     }) 
                 }else{
+                    that.data.imageName = result.data.photoUrl.replace(cosPath,'')
                     that.setData({
                         kaosheng_flg:"edit",
                         kaoshengInfo:result.data
@@ -78,44 +78,34 @@ Page({
     },
 
     check_message: function () {//{{{
-// ^[\u4E00-\u9FA5]{2,4}$
-// [1-9][0-9]{5}([1][9][0-9]{2}|[2][0][0|1][0-9])([0][1-9]|[1][0|1|2])([0][1-9]|[1|2][0-9]|[3][0|1])[0-9]{3}([0-9]|[X])$
-        // var regtel = new RegExp('(^1[3|4|5|7|8][0-9]{9}$)', 'g');
-        // let kaoshengInfo = this.data.kaoshengInfo
-        // if (kaoshengInfo.name == "") {
-        //     util.showModel("信息不全", "联系人没有填写");
-        //     return false
-        // } else if (kaoshengInfo.telphone == "") {
-        //     util.showModel("信息不全", "联系方式没有填写");
-        //     return false
-        // } else if (kaoshengInfo.content == "") {
-        //     util.showModel("信息不全", "发布内容不能为空");
-        //     return false
-        // }
-        // if (!regtel.exec(kaoshengInfo.telphone)) { //电话不正确不能通过
-        //     util.showModel('格式错误', '手机格式错误')
-        //     return false
-        // }
-        // this.data.kaoshengInfo.images_name =  this.data.imageNameList.join(",")
-        // return true 
+        var nameRegx = new RegExp('^[\u4E00-\u9FA5]{2,4}$','g');
+        var sfzRegx = new RegExp('[1-9][0-9]{5}([1][9][0-9]{2}|[2][0][0|1][0-9])([0][1-9]|[1][0|1|2])([0][1-9]|[1|2][0-9]|[3][0|1])[0-9]{3}([0-9]|[X])$','g');
+        var telphoneRegx = new RegExp('(^1[3|4|5|7|8][0-9]{9}$)', 'g');
+        let kaoshengInfo = this.data.kaoshengInfo
+        //this.data.kaoshengInfo.photoUrl =  this.data.imageNameList.join(",")
+        if (!nameRegx.exec(kaoshengInfo.name)) {
+            util.showModel("输入有误", "名字只能是中文");
+            return false
+        } else if (!telphoneRegx.exec(kaoshengInfo.telphone)) {
+            util.showModel("信息格式错误", "电话填写错误");
+            return false
+        } else if (!sfzRegx.exec(kaoshengInfo.sfzid)) {
+            util.showModel("信息不全", "身份证填写错误");
+            return false
+        } else if(kaoshengInfo.photoUrl == "null"){
+            util.showModel("信息不全", "请上传一寸照");
+            return false
+        }
+        return true 
     },//}}}
 
-    //delete_img_by
-    //非常规操作，既没有点击发布也没有点击取消，或者是被打断，应该要清理cos中的数据
-    //否则会产生很多僵尸图片
-    clear_cos:function (){//{{{
-        for(let i=0;i<this.data.imageNameList.length;i++){
-            this.delete_cos_img(this.data.imageNameList[i])
-        }
-    },//}}}
 
     cancle: function () {//{{{
         if(this.data.kaosheng_flg === "new"){//新建信息放弃发布，删除所有已经上传图片,修改的话不能删除
             this.clear_cos()
         }
-        this.jump()
+        this.jump()//返回上级页面
     },//}}}
-
 
     //在列表中删除图片并setData
     image_display:function (idx){//{{{
@@ -126,54 +116,6 @@ Page({
         })
     },//}}}
 
-
-    //删除图片1删除cos 2如果是editMsg还需要去掉DB中的图片
-    delete_img:function (event){//{{{
-        var that = this
-        var idx = event.currentTarget.dataset.idx
-        var img_name = this.data.imageNameList[idx]
-        this.delete_cos_img(img_name)
-        if(this.data.kaosheng_flg === "edit"){//如果是修改消息，就必须修改数据库
-            this.delete_db_img(this.data.kaoshengInfo.msg_id , img_name )
-        }
-        this.image_display(idx)
-    },//}}}
-
-    //从cos中删除图片，一次一张
-    delete_cos_img:function (img_name){//{{{
-        var that = this
-        qcloud.request({
-            url: `${config.service.host}/community/delete_cos_img`,
-            data: {img_name:img_name},
-            method: 'POST',
-            header: { 'content-type':'application/x-www-form-urlencoded' },
-            success(result) {
-            },
-            fail(error) {
-                util.showModel('请求失败', error);
-                console.log('request fail', error);
-            }
-        })
-    },//}}}
-
-    //从数据库中删除图片存储
-    delete_db_img:function (msg_id,img_name){//{{{
-        var that = this
-        qcloud.request({
-            url: `${config.service.host}/kaoshengInfo/delete_db_img`,
-            data: {
-                open_id:that.data.userInfo.openId,
-            },
-            method: 'POST',
-            header: { 'content-type':'application/x-www-form-urlencoded' },
-            success(result) {
-            },
-            fail(error) {
-                util.showModel('请求失败', error);
-                console.log('request fail', error);
-            }
-        })
-    },//}}}
 
     //用户点击发布提交表单
     submit:function (){//{{{
@@ -207,7 +149,7 @@ Page({
         })
     },//}}}
 
-    //发布消息
+    //新建考生信息
     new_kaosheng: function () {//{{{
         let that = this
         let kaoshengInfo = that.data.kaoshengInfo
@@ -226,51 +168,15 @@ Page({
         })
     },//}}}
 
-    delete_local_img_by_filepath:function(filepath){
-        for(let i=0;i<this.data.localImagePaths.length;i++){
-            if(filepath === this.data.localImagePaths[i]){
-                this.data.localImagePaths.splice(i,1)
-            }
-        }
-    },
-    //上传图片，分为两个子操作，upload_Cos,img_to_db
-    //新上传到cos cos返回图片名称 再同步到db中
-    upload_cos:function (filePath,idx){//{{{
-        let that = this
-        //上传图片
-        wx.uploadFile({
-            url: config.service.uploadUrl,
-            filePath: filePath,
-            name: 'file',
-            success: function(res) {
-                res = JSON.parse(res.data)
-                if(res.data === 'mingantu'){
-                    that.delete_local_img_by_filepath(filePath)
-                    that.setData({
-                        localImagePaths:that.data.localImagePaths
-                    })
-                    util.showModel("图片不规范", "请遵守国家法律法规,重新上传!") 
-                }else{
-                    that.data.imageNameList.push(res.data.name)
-                    if(that.data.kaosheng_flg === "edit"){
-                        that.img_to_db(that.data.kaoshengInfo.msg_id , res.data.name )
-                    }
-                }
-            },
-            fail: function(e) {
-                util.showModel('上传图片失败')
-            }
-        })//end_uploadFile
-    },//}}}
 
     //新上传的图片放到db中
-    img_to_db:function (msg_id,img_name){//{{{
+    img_to_db:function (open_id,imgUrl){//{{{
         //请求后台update图片数据
         qcloud.request({
-            url: `${config.service.host}/community/db_add_img`,
+            url: `${config.service.host}/baoming/kaoshengInfo/update_db_img`,
             data: {
-                msg_id:msg_id,
-                img_name:img_name
+                open_id:open_id,
+                imgUrl:imgUrl
             },
             method: 'POST',
             header: { 'content-type':'application/x-www-form-urlencoded' },
@@ -283,43 +189,76 @@ Page({
         })
     },//}}}
 
-    //修改信息，用户上传图片
+    //修改信息，用户上传图片,本文件中只允许上传一张照片
     chooseImage: function (event) {//{{{
         var that = this;
-        let remain = 6 - this.data.localImagePaths.length
-        if(remain > 0){
-            wx.chooseImage({
-                count: remain, // 一次最多可以选择2张图片一起上传
-                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                success: function (res) {
-                    that.data.localImagePaths = that.data.localImagePaths.concat(res.tempFilePaths);
-                    that.setData({
-                        localImagePaths:that.data.localImagePaths 
-                    });
-                    //文件路径最新选择的所有图片
-                    var filePath = res.tempFilePaths
-                    that.data.tempFilePaths = res.tempFilePaths
-                    //文件上传中禁止点发布，否则上传不完整
-                    that.pub_lock = "lock"
-                    util.showBusy('正在上传')
-                    for(var i=0;i<filePath.length;i++){
-                        that.upload_cos(filePath[i],i)
-                    }
-                    //wx.hideToast()
-                    that.pub_lock = "unlock"
-                }
-            })
-        }
+        wx.chooseImage({
+            count: 1, // 一次最多可以选择2张图片一起上传
+            sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                that.data.localImagePath = res.tempFilePaths[0];
+                that.setData({
+                    localImagePath:that.data.localImagePath
+                });
+                //文件上传中禁止点发布，否则上传不完整
+                that.pub_lock = "lock"
+                util.showBusy('正在上传')
+                that.upload_cos(that.data.localImagePath)
+                that.pub_lock = "unlock"
+            }
+        })
     },//}}}
 
-    previewImage: function (e) {//{{{
-        var that = this;
-        var dataid = e.currentTarget.dataset.id;
-        var localImagePaths = that.data.localImagePaths;
+    //新上传到cos cos返回图片名称 再同步到db中
+    upload_cos:function (filePath){//{{{
+        let that = this
+        wx.uploadFile({
+            url: config.service.uploadUrl,
+            filePath: filePath,
+            name: 'file',
+            success: function(res) {
+                res = JSON.parse(res.data)
+                that.delete_cos_img(that.data.imageName)//删除上一张图片
+                that.data.imageName = res.data.name
+                if(that.data.kaosheng_flg === "edit"){//如果是编辑态，需要更新db，前面已经删掉了原图片
+                    that.img_to_db(that.data.kaoshengInfo.open_id , res.data.imgUrl)
+                }
+                that.data.kaoshengInfo.photoUrl = res.data.imgUrl
+                that.setData({
+                    kaoshengInfo:that.data.kaoshengInfo
+                })
+                wx.hideToast()
+            },
+            fail: function(e) {
+                util.showModel('上传图片失败')
+            }
+        })//end_uploadFile
+    },//}}}
+    //从cos中删除图片，一次一张
+    delete_cos_img:function (img_name){//{{{
+        var that = this
+        qcloud.request({
+            url: `${config.service.host}/baoming/kaoshengInfo/delete_cos_img`,
+            data: {img_name:img_name},
+            method: 'POST',
+            header: { 'content-type':'application/x-www-form-urlencoded' },
+            success(result) {
+                console.log(result)
+            },
+            fail(error) {
+                util.showModel('请求失败', error);
+                console.log('request fail', error);
+            }
+        })
+    },//}}}
+
+    previewImage: function () {//{{{
+        var localImagePaths = [];
+        localImagePaths[0] = this.data.kaoshengInfo.photoUrl;
         wx.previewImage({
-            current: localImagePaths[dataid],
-            urls: this.data.localImagePaths
+            current: 0,
+            urls: localImagePaths
         });
     },//}}}
 
