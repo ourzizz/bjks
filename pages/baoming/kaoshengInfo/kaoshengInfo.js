1c652db452fa78704367c30c03a45b27f5594855
ourzizz <552716481@qq.com>
2020-01-10 15:41:40 +0800

    试下vscode的提交功能

--- pages/baoming/kaoshengInfo/kaoshengInfo.js
+++ pages/baoming/kaoshengInfo/kaoshengInfo.js
@@ -1,3 +1,6 @@
+/**
+ * 考生信息填写点击保存后姓名和身份证就变成不可修改字段
+ */
 var qcloud = require('../../../vendor/wafer2-client-sdk/index')
 var config = require('../../../config')
 var util = require('../../../utils/util.js')
@@ -6,38 +9,39 @@ var cosPath = "http://bjks-1252192276.cos.ap-chengdu.myqcloud.com"
 var app = getApp()
 Page({
     data: {
+        ksid: '',
+        step:1,
         activeIndex: -1,
         kaosheng_flg:"new",
         kaoshengInfo: {
             photoUrl:"null"
         },
-        tree_list:[],
+        kaoshengInfoCopy:{},//考生副本用于对比原始数据找出修改部分
         userInfo:{},
         logged:false,
         localImagePath:'', //小程序wxml页面显示的图片列表,如果是修改那么就是从服务器请求的url，否则为本地选中的图片列表
         imageName:'', //upload返回的是url和name，以前开发保留了name用逗号连接存到数据库，这个列表是存储使用的,服务器发来的namelist
         tempFilePath:'', //待上传列表
         pub_lock:'unlock',//发布锁，避免用户多次点击 unlock lock
-        minganStatus:true,
-        layer:[],
-        zhiweiPath:[],
+        tree_list:[],//职位表树形结构列表
+        layer:[], //存储层级代码长度
+        baomingInfo:{},//给后台增改(一旦报名不能删除) 包括报考职位代码 确认状态 审核情况 未通过原因
+        baomingInfoCopy:{},//副本 涉及到改的都需要副本对比
+        zhiweiPath:[],//view考生只管查看已经勾选的报考地区-单位-职位信息
+        operas:[
+            {step:1,name:"信息填写"},
+            {step:2,name:"报名确认"},
+            {step:3,name:"查看信息"},
+        ]
     },
-
-    onLoad: function (options) {//{{{ 需要向后台请求考生基本信息 和报考信息
+    init_page:function(openId,ksid){
         let that = this
-        const session = qcloud.Session.get()
-        if (session) { //session存在
-            this.setData({
-                userInfo: session.userinfo,
-                logged: true
-            })
-        }
         util.showBusy("下载数据中")
         qcloud.request({
             url: `${config.service.host}/baoming/kaoshengInfo/get_kaosheng_kaoshi`,
             data: {
-                open_id:options.openId,
-                ksfileid:options.ksid
+                open_id:openId,
+                ksfileid:ksid
             },
             method: 'POST',
             header: { 'content-type':'application/x-www-form-urlencoded' },
@@ -46,16 +50,26 @@ Page({
                 if(result.data.kaoshengInfo == "null"){
                     that.setData({
                         kaosheng_flg:"new",
-                        kaoshengInfo:{}
+                        kaoshengInfo:{},
+                        baomingInfo:{open_id:openId,ksid:ksid,bmconfirm:0}
                     }) 
                 }else{
                     that.data.imageName = result.data.kaoshengInfo.photoUrl.replace(cosPath,'')
                     that.data.tree_list = result.data.zhiwei
                     that.init_tree_list(that.data.tree_list)
+                    if (result.data.baomingInfo === null) {//有考生信息 无报名信息=>考生以往报过其他考试 本考试未报名,报名未确认
+                        result.data.baomingInfo = {open_id:openId,ksid:ksid,code:"",bmconfirm:0}
+                    }else{
+                        that.getPath(result.data.baomingInfo.code)
+                    }
+                    that.data.baomingInfoCopy = JSON.parse(JSON.stringify(result.data.baomingInfo));
+                    that.data.kaoshengInfoCopy = JSON.parse(JSON.stringify(result.data.kaoshengInfo));
                     that.setData({
                         kaosheng_flg:"edit",
                         kaoshengInfo:result.data.kaoshengInfo,
-                        tree_list: that.data.tree_list
+                        tree_list: that.data.tree_list,
+                        baomingInfo:result.data.baomingInfo,
+                        zhiweiPath:that.data.zhiweiPath
                     }) 
                 }
             },
@@ -64,6 +78,18 @@ Page({
                 console.log('request fail', error);
             }
         })
+    },
+    onLoad: function (options) {//{{{ 需要向后台请求考生基本信息 和报考信息
+        let that = this
+        this.data.ksid = options.ksid
+        const session = qcloud.Session.get()
+        if (session) { //session存在
+            this.setData({
+                userInfo: session.userinfo,
+                logged: true
+            })
+        }
+        this.init_page(options.openId,options.ksid)
     },//}}}
 
     init_tree_list:function(tree_list){
@@ -130,6 +156,34 @@ Page({
         });
     },
 
+
+    set_idx: function (event) {
+        var idx = event.currentTarget.dataset.idx
+        this.setData({
+            activeIndex: idx
+        })
+    },
+
+    radioChange: function (e) {
+        var path = []
+        var idx = this.data.activeIndex
+        var code = e.detail.value
+        this.getPath(code)
+        this.data.tree_list.forEach(element => {
+            if(element.code !== code){
+                element.checked = false
+            }else{
+                element.checked = true
+            }
+            this.init_tree_list()
+        });
+        this.data.baomingInfo.code = code
+        this.setData({
+            tree_list:this.data.tree_list,
+            zhiweiPath:this.data.zhiweiPath,
+        })
+    },
+
     getPath:function(code){ //"1240202201"
         var layer = this.data.layer 
         var tempCode = ""
@@ -144,18 +198,6 @@ Page({
         }
     },
 
-    set_idx: function (event) {
-        var idx = event.currentTarget.dataset.idx
-        this.setData({
-            activeIndex: idx
-        })
-    },
-
-    checkboxChange: function (e) {
-        var path = []
-        console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-        this.getPath(e.detail.value[0])
-    },
     baoming_confirm: function () { },
 
     input_name: function (e) {//{{{
@@ -179,7 +221,6 @@ Page({
     input_major:function (e){
         this.data.kaoshengInfo.major = e.detail.value
     },
-
     check_message: function () {//{{{
         var nameRegx = new RegExp('^[\u4E00-\u9FA5]{2,4}$','g');
         var sfzRegx = new RegExp('[1-9][0-9]{5}([1][9][0-9]{2}|[2][0][0|1][0-9])([0][1-9]|[1][0|1|2])([0][1-9]|[1|2][0-9]|[3][0|1])[0-9]{3}([0-9]|[X])$','g');
@@ -197,6 +238,9 @@ Page({
             return false
         } else if(kaoshengInfo.photoUrl == "null"){
             util.showModel("信息不全", "请上传一寸照");
+            return false
+        } else if(this.data.zhiweiPath.length === 0){
+            util.showModel("信息不全", "考生未选职位或科目");
             return false
         }
         return true 
@@ -223,18 +267,63 @@ Page({
         }
     },//}}}
 
+    get_modify:function(){//得到考生修改考生信息 和职位信息
+        var bmCopy = this.data.baomingInfoCopy
+        var bmOrigin = this.data.baomingInfo
+        var ksCopy = this.data.kaoshengInfo
+        var ksOrigin = this.data.kaoshengInfoCopy
+        var Modify = {bmModify:{},ksModify:{}}
+        if(bmCopy.code !== bmOrigin.code){
+            Modify.bmModify = this.data.baomingInfo
+        }
+        if(ksCopy.photoUrl !== ksOrigin.photoUrl){
+            Modify.ksModify.photoUrl = ksCopy.photoUrl
+        }
+        if(ksCopy.telphone !== ksOrigin.telphone){
+            Modify.ksModify.telphone = ksCopy.telphone
+        }
+        if(ksCopy.school !== ksOrigin.school){
+            Modify.ksModify.school = ksCopy.school
+        }
+        if(ksCopy.degree !== ksOrigin.degree){
+            Modify.ksModify.degree = ksCopy.degree
+        }
+        if(ksCopy.education !== ksOrigin.education){
+            Modify.ksModify.education = ksCopy.education
+        }
+        if(ksCopy.major !== ksOrigin.major){
+            Modify.ksModify.major = ksCopy.major
+        }
+        if(util.isEmptyObject(Modify)){
+            return "NULL";
+        }
+        this.data.pub_lock = "unlock" //有修改就要开锁
+        return Modify;
+    },
+
     //保存修改的信息
     update_kaosheng:function (){//{{{
         var that = this
+        var Modify = this.get_modify()
+        util.showBusy("正在更新数据")
         qcloud.request({
             url: `${config.service.host}/baoming/kaoshengInfo/update_kaosheng`,
             data: {
-                kaoshengInfo: JSON.stringify(this.data.kaoshengInfo)
+                open_id: that.data.userInfo.openId,
+                kaoshengInfo: JSON.stringify(Modify.ksModify),
+                baomingInfo: JSON.stringify(Modify.bmModify)
             },
             method: 'POST',
             header: { 'content-type': 'application/x-www-form-urlencoded' },
-            success(result) {
-                that.jump()
+            success(result) {//更新后 更新所有副本
+                that.init_page(that.data.userInfo.openId,that.data.ksid)
+                // that.data.baomingInfoCopy = JSON.parse(JSON.stringify(that.data.baomingInfo));
+                // that.data.kaoshengInfoCopy = JSON.parse(JSON.stringify(that.data.kaoshengInfo));
+                // that.setData({//bug修改了信息set step后又回到未修改的数据
+                //     baomingInfo:that.data.baomingInfo,
+                //     kaoshengInfo:that.data.baokaoshengInfo
+                // })
+                wx.hideToast()
             },
             fail(error) {
                 that.jump()
@@ -250,7 +339,8 @@ Page({
         qcloud.request({
             url: `${config.service.host}/baoming/kaoshengInfo/store_kaosheng`,
             data: {
-                kaoshengInfo: JSON.stringify(kaoshengInfo)
+                kaoshengInfo: JSON.stringify(kaoshengInfo),
+                baomingInfo: JSON.stringify(this.data.baomingInfo)
             },
             method: 'POST',
             header: { 'content-type': 'application/x-www-form-urlencoded' },
@@ -357,4 +447,10 @@ Page({
 
     jump:function (){
     },
+    set_step:function(event){
+        var step = event.currentTarget.dataset.step
+        this.setData({
+            step:step
+        })
+    }
 })
